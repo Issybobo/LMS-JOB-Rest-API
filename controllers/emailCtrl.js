39fb198ab4845fc992fce0c1eb02f0a5c9dad93a
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-04-15 16:44:54 +0100

    added email controller

---
+++ controllers/emailCtrl.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+const asyncHandler = require("express-async-handler");
+
+const sendEmail = asyncHandler(async(data, req, res) => {
+    let transporter = nodemailer.createTransport({
+        host: "stmp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_ID,
+            pass: process.env.MAIL_PASS,
+
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: "Developer's Corner",
+        to: data.to,
+        subject: data.subject,
+        text: data.text,
+        html: data.html,
+    })
+    console.log("Messsage Send: ", info.messageId);
+    console.log("Preview Url:", nodemailer.getTestMessageUrl(info));
+})
+
+module.exports = sendEmail;
